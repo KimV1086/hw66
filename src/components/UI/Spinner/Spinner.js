354cc10a9f463bbc6cbce3e8e36159f9961a73c5
Vitalik Kim <kimv1086@gmail.com>
2020-01-04 22:32:40 +0600

    add spinner

---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Spinner.css';
+
+const Spinner = ({show}) => {
+    if (show) {
+        return <div className="Spinner">Loading...</div>
+    }
+
+    return null;
+};
+
+export default Spinner;
