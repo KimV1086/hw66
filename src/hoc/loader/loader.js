86d65076a09182a7c554e451db32983431faefa0
Vitalik Kim <kimv1086@gmail.com>
2020-01-04 22:34:40 +0600

    add hoc loader

---
+++ src/hoc/loader/loader.js
@@ -0,0 +1,38 @@
+import React, {Component, Fragment} from 'react';
+import Spinner from "../../components/UI/Spinner/Spinner";
+
+const loader = (WrappedComponent, axios) => {
+    return class LoaderHOC extends Component {
+
+        constructor(props) {
+            super(props);
+
+            this.state = {
+                loading: true
+            };
+
+            this.state.interceptorId = axios.interceptors.response.use(res => {
+                console.log('loaded');
+                this.setState({loading: false});
+                return res;
+            }, err => err);
+        }
+
+        componentWillUnmount() {
+            axios.interceptors.response.eject(this.state.interceptorId);
+        }
+
+
+        render() {
+            return (
+                <Fragment>
+                    <Spinner show={this.state.loading}/>
+                    <WrappedComponent {...this.props}/>
+                </Fragment>
+            )
+        }
+
+    }
+};
+
+export default loader;
